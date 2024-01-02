9f6d4f91007de8931dfc39d2057517bba938b895
danielOuattara <daniel.ouattara@gmx.com>
2024-01-02 16:57:38 +0100

    adding "users" module & routes

--- _01_nestjs_crash_course/src/users/controllers/users/users.controller.ts
+++ _01_nestjs_crash_course/src/users/controllers/users/users.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get()
+  getUser() {
+    return { username: 'John Doe' };
+  }
+
+  @Get('posts')
+  getUserPosts() {
+    return [
+      {
+        username: 'John',
+        email: 'john@email.com',
+        posts: [
+          { id: 1, title: 'posts 1' },
+          { id: 2, title: 'posts 2' },
+        ],
+      },
+    ];
+  }
+
+  @Get('posts/comments')
+  getUserPostsComments() {
+    return [
+      {
+        posts: [
+          { id: 1, title: 'comment on posts 1' },
+          { id: 2, title: 'comment on posts 2' },
+        ],
+      },
+    ];
+  }
+}
