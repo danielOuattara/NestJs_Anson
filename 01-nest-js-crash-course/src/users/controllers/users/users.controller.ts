9761c626dde2be9d16712a12d956815f4351ab2a
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-10-28 10:08:12 +0100

    01 NestJS crash course:  Handling Routes

--- 01-nest-js-crash-course/src/users/controllers/users/users.controller.ts
+++ 01-nest-js-crash-course/src/users/controllers/users/users.controller.ts
@@ -1,4 +1,42 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get()
+  getUsers() {
+    return [
+      { id: 1, username: 'John', email: 'john@email.com' },
+      { id: 2, username: 'Ann', email: 'ann@email.com' },
+    ];
+  }
+
+  @Get('posts')
+  getUsersPosts() {
+    return [
+      {
+        postOwnerId: 1,
+        posts: [
+          { id: 1, title: 'Post 1' },
+          { id: 2, title: 'Post 2' },
+        ],
+      },
+    ];
+  }
+  @Get('/posts/comments')
+  getUsersPostsComments() {
+    return [
+      {
+        id: 1,
+        commentOwnerId: 1,
+        postOwnerId: 1,
+        title: 'Post 1 is perfect',
+      },
+      {
+        id: 1,
+        commentOwnerId: 1,
+        postOwnerId: 1,
+        title: 'Post 1 was not as perfect as expected',
+      },
+    ];
+  }
+}
