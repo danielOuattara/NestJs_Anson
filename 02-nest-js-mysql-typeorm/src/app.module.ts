30ec8b71005fa0528223cf0fac557740f9930f26
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-11-01 22:30:27 +0100

    02 NestJS MySQL TypeORM: connect to DB + setup .env

--- 02-nest-js-mysql-typeorm/src/app.module.ts
+++ 02-nest-js-mysql-typeorm/src/app.module.ts
@@ -1,9 +1,54 @@
+// import { Module } from '@nestjs/common';
+// import { AppController } from './app.controller';
+// import { AppService } from './app.service';
+// import { ConfigModule } from '@nestjs/config';
+// import { TypeOrmModule } from '@nestjs/typeorm';
+
+// @Module({
+//   imports: [
+//     ConfigModule.forRoot({ isGlobal: true }),
+//     TypeOrmModule.forRoot({
+//       type: type_value,
+//       host: host_value,
+//       port: port_value,
+//       username: name_value,
+//       password: password_value,
+//       database: database_name,
+//       entities: [],
+//       synchronize: true,
+//     }),
+//   ],
+//   controllers: [AppController],
+//   providers: [AppService],
+// })
+// export class AppModule {}
+
+//------------------------------------------------------------
+
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }), // Ensure .env is loaded globally
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('DATABASE_HOST'),
+        port: configService.get<number>('DATABASE_PORT'),
+        username: configService.get<string>('DATABASE_USER'),
+        password: configService.get<string>('DATABASE_PASSWORD'),
+        database: configService.get<string>('DATABASE_NAME'),
+        entities: [], // Add your entities here
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
