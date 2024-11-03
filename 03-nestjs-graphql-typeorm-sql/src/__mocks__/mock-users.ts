fc596099439e4c5138860195dee54351b3ae4613
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-11-03 23:52:34 +0100

    03 NestJS GraphQL TypeORM SQL :  Graphql playground

---
+++ 03-nestjs-graphql-typeorm-sql/src/__mocks__/mock-users.ts
@@ -0,0 +1,5 @@
+export const mockUsers = [
+  { id: 1, username: 'John', displayName: 'John Doe' },
+  { id: 2, username: 'Ann', displayName: 'Ann Waters' },
+  { id: 3, username: 'Mike', displayName: 'Mike Tyson' },
+];
--- 03-nestjs-graphql-typeorm-sql/src/graphql/resolvers/UserResolver.ts
+++ 03-nestjs-graphql-typeorm-sql/src/graphql/resolvers/UserResolver.ts
@@ -1,10 +1,22 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Int, Query, Resolver } from '@nestjs/graphql';
 import { UserModel } from '../models/UserModel';
+import { mockUsers } from 'src/__mocks__/mock-users';
 
 @Resolver()
 export class UserResolver {
   @Query((returns) => UserModel)
   getUser() {
     return { id: 1, username: 'John', displayName: 'John the John' };
+  }
+
+  @Query((returns) => UserModel, { nullable: true, name: 'userById' })
+  getUserById(@Args('id', { type: () => Int }) id: number) {
+    const user = mockUsers.find((user) => user.id === id);
+    return user;
+  }
+
+  @Query((returns) => [UserModel], { nullable: true })
+  getUsers() {
+    return mockUsers;
   }
 }
